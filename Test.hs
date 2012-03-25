36334115108ad44450a2b6bc804d760678963aa7
Ben Leslie <benno@benno.id.au>
2012-03-25 11:11:07 +1100

    Simple example of round tripping a trivial Haskell object

--- Test.hs
+++ Test.hs
@@ -4,11 +4,32 @@
 module Test where
 
 import Foreign.C.Types
+import Foreign.StablePtr
  
 hsfun :: CInt -> IO CInt
 hsfun x = do
     putStrLn "Hello World"
     return (42 + x)
 
+
+--extract :: Example -> CInt
+--extract (Foo x) = x
+
+data Wrapper = Wrap CInt
+
+wrap :: CInt -> IO (StablePtr Wrapper)
+wrap x = newStablePtr $ Wrap x
+
+unwrap :: StablePtr Wrapper -> IO CInt
+unwrap wrapped = do
+  Wrap d <- deRefStablePtr wrapped
+  return d
+
 foreign export ccall
     hsfun :: CInt -> IO CInt
+
+foreign export ccall
+    wrap :: CInt -> IO (StablePtr Wrapper)
+
+foreign export ccall
+    unwrap :: StablePtr Wrapper -> IO CInt
--- test.py
+++ test.py
@@ -4,3 +4,6 @@ lib = CDLL("./libTest.so")
 print lib
 print lib.hsfun
 print lib.hsfun(5)
+
+lib.wrap.restype = c_void_p
+print lib.unwrap(lib.wrap(2000))
